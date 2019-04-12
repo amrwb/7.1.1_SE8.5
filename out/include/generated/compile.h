0153badbc2541f1b9cf5f08d65e2a8af369e0f21
amrwb <yibyuk@icloud.com>
2019-04-12 10:56:25 +0900

    Tried to make bootable but failed

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Fri Mar 15 11:03:21 KST 2019"
+#define UTS_VERSION "#10 SMP PREEMPT Thu Apr 11 21:32:41 KST 2019"
 #define LINUX_COMPILE_BY "volte"
 #define LINUX_COMPILE_HOST "volte-HP-Notebook"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.49-g5ad1f76c-dirty"
+#define UTS_RELEASE "3.10.49-gc90b92ab-dirty"
